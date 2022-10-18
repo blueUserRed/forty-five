1d75918c8b679a3e3b1c3116a0db3fb3533d3da3
Marvin Kurka <8013@htl.rennweg.at>
2022-10-18 11:12:55 +0200

    did initial setup

---
+++ core/src/com/fourinachamber/fourtyfive/FourtyFive.kt
@@ -0,0 +1,27 @@
+package com.fourinachamber.fourtyfive
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
+
+class FourtyFive : ApplicationAdapter() {
+    var batch: SpriteBatch? = null
+    var img: Texture? = null
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(1f, 0f, 0f, 1f)
+        batch!!.begin()
+        batch!!.draw(img, 0f, 0f)
+        batch!!.end()
+    }
+
+    override fun dispose() {
+        batch!!.dispose()
+        img!!.dispose()
+    }
+}
