5c5d9615118a4cea34bbd0d4f308b9a3b86b69b8
Marvin Kurka <8013@htl.rennweg.at>
2022-12-15 15:51:05 +0100

    add BulletSelectors to make effects more dynamic, added a PutCardInHand effect

--- core/src/com/fourinachamber/fourtyfive/game/OnjExtensions.kt
+++ core/src/com/fourinachamber/fourtyfive/game/OnjExtensions.kt
@@ -11,10 +11,11 @@ object OnjExtensions {
             OnjEffect(Effect.ReserveGain(triggerOrError(it[0].value as String), (it[1].value as Long).toInt()))
         },
 
-        OnjFunction("buffDmg", listOf(OnjString::class, OnjInt::class)) {
+        OnjFunction("buffDmg", listOf(OnjString::class, OnjBulletSelector::class, OnjInt::class)) {
             OnjEffect(Effect.BuffDamage(
                 triggerOrError(it[0].value as String),
-                (it[1].value as Long).toInt()
+                (it[2].value as Long).toInt(),
+                (it[1] as OnjBulletSelector).value
             ))
         },
 
@@ -34,6 +35,45 @@ object OnjExtensions {
 
         OnjFunction("destroy", listOf(OnjString::class)) {
             OnjEffect(Effect.Destroy(triggerOrError(it[0].value as String)))
+        },
+
+        OnjFunction("putCardInHand", listOf(OnjString::class, OnjString::class)) {
+            OnjEffect(Effect.PutCardInHand(
+                triggerOrError(it[0].value as String),
+                it[1].value as String,
+                (it[2] as OnjInt).value.toInt(),
+            ))
+        },
+
+        /////////////////////////////////////////////////////////////////////////
+
+        OnjFunction("bNum", listOf(OnjArray::class)) { params ->
+            val onjArr = params[0] as OnjArray
+            val nums = mutableSetOf<Int>()
+            var allowSelf = false
+
+            for (value in onjArr.value) when (value) {
+                is OnjInt -> nums.add(value.value.toInt())
+                is OnjString -> {
+                    if (value.value.lowercase() != "this") {
+                        throw RuntimeException("string '${value.value}' not allowed in bNum")
+                    }
+                    allowSelf = true
+                }
+                else -> throw RuntimeException("bNum only allows ints or strings!")
+            }
+
+
+            OnjBulletSelector { self, other, slot ->
+                // when self === other allowSelf must be true, even if the slot is correct
+                if (self === other) allowSelf
+                else nums.contains(slot)
+            }
+        },
+
+        OnjFunction("bSelectByName", listOf(OnjString::class)) {
+            val name = it[0].value as String
+            OnjBulletSelector { _, other, _ -> other.name == name }
         },
 
         /////////////////////////////////////////////////////////////////////////
@@ -83,6 +123,16 @@ object OnjExtensions {
     ) : OnjValue() {
 
         override fun toString(): String = "'__effect__'"
+        override fun toString(indentationLevel: Int): String = toString()
+        override fun toJsonString(): String = toString()
+        override fun toJsonString(indentationLevel: Int): String = toString()
+
+    }
+    class OnjBulletSelector(
+        override val value: BulletSelector
+    ) : OnjValue() {
+
+        override fun toString(): String = "'__bullet-selector__'"
         override fun toString(indentationLevel: Int): String = toString()
         override fun toJsonString(): String = toString()
         override fun toJsonString(indentationLevel: Int): String = toString()
