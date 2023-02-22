e6a341c5472c8309fcb2a6828b15778747849da8
Marvin Kurka <8013@htl.rennweg.at>
2023-02-22 09:07:55 +0100

    mode basic node visualization possible, graphics are still missing and lines are broken

--- core/src/com/fourinachamber/fourtyfive/map/DetailMapWidget.kt
+++ core/src/com/fourinachamber/fourtyfive/map/DetailMapWidget.kt
@@ -1,16 +1,21 @@
 package com.fourinachamber.fourtyfive.map
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.ui.Widget
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable
+import com.fourinachamber.fourtyfive.screen.general.OnjScreen
 import com.fourinachamber.fourtyfive.screen.general.ZIndexActor
 import com.fourinachamber.fourtyfive.utils.plus
 
 class DetailMapWidget(
-    val nodeDrawable: Drawable,
+    private val screen: OnjScreen,
+    private val nodeDrawable: Drawable,
+    private val nodeSize: Float,
+    private val lineWidth: Float,
     var background: Drawable? = null
 ) : Widget(), ZIndexActor {
 
@@ -41,19 +46,23 @@ class DetailMapWidget(
         val uniqueNodes = uniqueNodes ?: return
         val uniqueEdges = uniqueEdges ?: return
         for (node in uniqueNodes) {
-            nodeDrawable.draw(batch, x + node.x, y + node.y, 1f, 1f)
+            nodeDrawable.draw(batch, x + node.x, y + node.y, nodeSize, nodeSize)
         }
         val globalCoords = localToStageCoordinates(Vector2(0f, 0f))
+        batch.end()
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
+        shapeRenderer.projectionMatrix = screen.stage.viewport.camera.combined
         shapeRenderer.color = Color.BLACK
+        Gdx.gl.glLineWidth(Gdx.graphics.height / lineWidth)
         for (edge in uniqueEdges) {
             val (node1, node2) = edge
             shapeRenderer.line(
-                globalCoords + Vector2(node1.x, node1.y),
-                globalCoords + Vector2(node2.x, node2.y)
+                globalCoords + Vector2(node1.x + nodeSize / 2, node1.y + nodeSize / 2),
+                globalCoords + Vector2(node2.x + nodeSize / 2, node2.y + nodeSize / 2)
             )
         }
         shapeRenderer.end()
+        batch.begin()
     }
 
 }
