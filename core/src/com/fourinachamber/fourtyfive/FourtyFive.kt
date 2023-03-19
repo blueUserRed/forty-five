52849bcedf01bef52525b5b5901b6688d37705c1
Marvin Kurka <8013@htl.rennweg.at>
2023-03-19 01:24:35 +0100

    resources are now loaded in parallel on a separate thread

--- core/src/com/fourinachamber/fourtyfive/FourtyFive.kt
+++ core/src/com/fourinachamber/fourtyfive/FourtyFive.kt
@@ -15,15 +15,13 @@ import com.fourinachamber.fourtyfive.onjNamespaces.StyleNamespace
 import com.fourinachamber.fourtyfive.rendering.Renderable
 import com.fourinachamber.fourtyfive.screen.Resource
 import com.fourinachamber.fourtyfive.screen.ResourceManager
-import com.fourinachamber.fourtyfive.utils.AllThreadsAllowed
-import com.fourinachamber.fourtyfive.utils.FourtyFiveLogger
-import com.fourinachamber.fourtyfive.utils.MainThreadOnly
-import com.fourinachamber.fourtyfive.utils.TemplateString
+import com.fourinachamber.fourtyfive.utils.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import onj.customization.OnjConfig
+import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 /**
@@ -40,8 +38,11 @@ object FourtyFive : Game() {
 
     var currentGame: GameController? = null
 
+    val serviceThread: ServiceThread = ServiceThread()
+
     override fun create() {
         init()
+        serviceThread.start()
         if (generateCards) runCardGenerator()
         changeToScreen("screens/title_screen.onj")
     }
@@ -58,24 +59,11 @@ object FourtyFive : Game() {
 
     fun changeToScreen(screenPath: String, controllerContext: Any? = null) = runBlocking {
         val currentScreen = currentScreen
-        val screen: OnjScreen
-        val time = measureTimeMillis {
-            if (currentScreen?.transitionAwayTime != null) currentScreen.transitionAway()
-            screen = ScreenBuilder(Gdx.files.internal(screenPath)).build(controllerContext)
-        }
-//        println("screen building: $time")
-        // TODO: figure out how to load resources during the transition time
-        ResourceManager
-            .resources
-            .filter { it.state == Resource.ResourceState.NOT_LOADED && screen in it.borrowedBy }
-            .forEach {
-//                CoroutineScope
-                launch(Dispatchers.IO) {
-//                    println("preparing ${it.handle}")
-                    it.prepare()
-//                    println("finished preparing ${it.handle}")
-                }
-            }
+        if (currentScreen?.transitionAwayTime != null) currentScreen.transitionAway()
+        val screen = ScreenBuilder(Gdx.files.internal(screenPath)).build(controllerContext)
+
+        serviceThread.sendMessage(ServiceThreadMessage.PrepareResources)
+
         if (currentScreen == null) {
             FourtyFiveLogger.title("changing screen")
             this@FourtyFive.currentScreen = screen
@@ -91,27 +79,6 @@ object FourtyFive : Game() {
             }
         }
     }
-
-//    @MainThreadOnly
-//    fun changeToScreen(screen: OnjScreen) {
-//        val currentScreen = currentScreen
-//        if (currentScreen?.transitionAwayTime == null) {
-//            FourtyFiveLogger.title("changing screen")
-//            currentScreen?.dispose()
-//            this.currentScreen = screen
-//            currentRenderable = screen
-//            setScreen(screen)
-//            return
-//        }
-//        currentScreen.transitionAway()
-//        currentScreen.afterMs(currentScreen.transitionAwayTime) {
-//            FourtyFiveLogger.title("changing screen")
-//            currentScreen.dispose()
-//            this.currentScreen = screen
-//            currentRenderable = screen
-//            setScreen(screen)
-//        }
-//    }
 
     @AllThreadsAllowed
     fun useRenderPipeline(renderable: Renderable) {
@@ -143,6 +110,7 @@ object FourtyFive : Game() {
         MapManager.write()
         SaveState.write()
         currentScreen?.dispose()
+        serviceThread.close()
         ResourceManager.end()
         super.dispose()
     }
