7b89a2d050ab37676acfd46b9e9097c4d8c5f9db
felix <9202@htl.rennweg.at>
2023-06-16 18:54:48 +0200

    added a person with image

---
+++ core/src/com/fourinachamber/fortyfive/map/dialog/ShopWidget.kt
@@ -0,0 +1,8 @@
+package com.fourinachamber.fortyfive.map.dialog
+
+import com.fourinachamber.fortyfive.screen.general.CustomFlexBox
+import com.fourinachamber.fortyfive.screen.general.OnjScreen
+
+class ShopWidget(screen: OnjScreen) : CustomFlexBox(screen) {
+
+}
