6c9294ebd2e89d52d316053e62ed0ba35b3bad68
felix <9202@htl.rennweg.at>
2023-06-17 08:23:43 +0200

    safety commit with working onjschema for shops.onj

--- core/src/com/fourinachamber/fortyfive/map/shop/ShopScreenController.kt
+++ core/src/com/fourinachamber/fortyfive/map/shop/ShopScreenController.kt
@@ -10,44 +10,50 @@ import onj.parser.OnjParser
 import onj.parser.OnjSchemaParser
 import onj.schema.OnjSchema
 import onj.value.OnjArray
-import onj.value.OnjNamedObject
 import onj.value.OnjObject
 
-class ShopScreenController(onj: OnjObject): ScreenController() {
+class ShopScreenController(onj: OnjObject) : ScreenController() {
 
     private lateinit var screen: OnjScreen
     private lateinit var context: ShopMapEvent
-//    private val cardConfigFile = onj.get<String>("cardsFile")
+    private val personImageActorName = onj.get<String>("shopPersonImageActor")
+
+    //    private val cardConfigFile = onj.get<String>("cardsFile")
 //    private val cardDragAndDropBehaviour = onj.get<OnjNamedObject>("cardDragBehaviour")
 //    private val cardDrawActorName = onj.get<String>("cardDrawActor")
     lateinit var closeButton: Actor
-        private set
 
-    lateinit var destroyCardInstructionActor: Actor
-        private set
-
-//    private val shopWidgetName = onj.get<String>("shopWidgetName")
+    //    private val shopWidgetName = onj.get<String>("shopWidgetName")
     private val shopFilePath = onj.get<String>("shopsFile")
+
+
+    lateinit var personImageActor: Actor
     private lateinit var shopWidget: ShopWidget
 
     override fun init(onjScreen: OnjScreen, context: Any?) {
         screen = onjScreen
         if (context !is ShopMapEvent) throw RuntimeException("context for shopScreenController must be a shopMapEvent")
         this.context = context
-//        val shopWidget = screen.namedActorOrError(shopWidgetName)
+
+        println(personImageActorName)
+//        println(onj.get<String>(""))
+        personImageActor = screen.namedActorOrError(personImageActorName)
+        println(personImageActor)
 //        if (shopWidget !is ShopWidget) {
 //            throw RuntimeException("widget with name $shopWidgetName must be of type shopWidget")
 //        }
 //        this.shopWidget = shopWidget
-//        val shopFile = OnjParser.parseFile(Gdx.files.internal(shopFilePath).file())
-//        shopsSchema.assertMatches(shopFile)
-//        shopFile as OnjObject
-//        val shop = shopFile
-//            .get<OnjArray>("shops")
-//            .value
-//            .map { it as OnjObject }
-//            .find { it.get<String>("name") == context.person }
-//            ?: throw RuntimeException("unknown shop: ${context.person}")
+        val shopFile = OnjParser.parseFile(Gdx.files.internal(shopFilePath).file())
+        shopsSchema.assertMatches(shopFile)
+        shopFile as OnjObject
+        val person = shopFile
+            .get<OnjArray>("people")
+            .value
+            .map { it as OnjObject }
+            .find { it.get<String>("name") == context.person }
+            ?: throw RuntimeException("unknown shop: ${context.person}")
+
+        println(person)
 //        val shopOnj = shop.get<OnjObject>("shop")
 //        val shop2 = shop.readFromOnj(shopOnj, screen)
 //        shopWidget.start(shop2)
