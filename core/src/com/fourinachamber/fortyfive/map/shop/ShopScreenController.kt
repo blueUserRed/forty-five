ee0a5343b81676e48961d11579137c513ea16933
felix <9202@htl.rennweg.at>
2023-06-17 22:10:17 +0200

    added Parameters and prepared for PersonWidget (not yet added)

--- core/src/com/fourinachamber/fortyfive/map/shop/ShopScreenController.kt
+++ core/src/com/fourinachamber/fortyfive/map/shop/ShopScreenController.kt
@@ -4,12 +4,15 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import com.fourinachamber.fortyfive.map.detailMap.ShopMapEvent
+import com.fourinachamber.fortyfive.screen.ResourceManager
 import com.fourinachamber.fortyfive.screen.general.CustomImageActor
 //import com.fourinachamber.fortyfive.map.shop.Shop
 import com.fourinachamber.fortyfive.screen.general.OnjScreen
 import com.fourinachamber.fortyfive.screen.general.ScreenController
 import com.fourinachamber.fortyfive.screen.general.styles.StyledActor
+import com.fourinachamber.fortyfive.screen.general.styles.WidthStyleProperty
 import onj.parser.OnjParser
 import onj.parser.OnjSchemaParser
 import onj.schema.OnjSchema
@@ -23,6 +26,7 @@ class ShopScreenController(onj: OnjObject) : ScreenController() {
     private lateinit var screen: OnjScreen
     private lateinit var context: ShopMapEvent
     private val personImageActorName = onj.get<String>("shopPersonImageActor")
+    private lateinit var person: OnjObject
 
     //    private val cardConfigFile = onj.get<String>("cardsFile")
 //    private val cardDragAndDropBehaviour = onj.get<OnjNamedObject>("cardDragBehaviour")
@@ -31,10 +35,10 @@ class ShopScreenController(onj: OnjObject) : ScreenController() {
 
     //    private val shopWidgetName = onj.get<String>("shopWidgetName")
     private val shopFilePath = onj.get<String>("shopsFile")
-
+    private lateinit var personDrawable: Drawable
 
     lateinit var personImageActor: CustomImageActor
-    private lateinit var shopWidget: ShopWidget
+    private lateinit var personWidget: ShopWidget
 
     override fun init(onjScreen: OnjScreen, context: Any?) {
         screen = onjScreen
@@ -50,55 +54,41 @@ class ShopScreenController(onj: OnjObject) : ScreenController() {
         val shopFile = OnjParser.parseFile(Gdx.files.internal(shopFilePath).file())
         shopsSchema.assertMatches(shopFile)
         shopFile as OnjObject
-        val person = shopFile
+        person = shopFile
             .get<OnjArray>("people")
             .value
             .map { it as OnjObject }
             .find { it.get<String>("name") == context.person }
             ?: throw RuntimeException("unknown shop: ${context.person}")
 
-//        println(person)
-//        val imgData=person.get<OnjObject>("image").value
-//        personImageActor.setPosition(imgData["positionLeft"]?.value as Float, imgData["positionTop"]?.value as Float)
-//        personImageActor.setSize(imgData["width"]?.value as Float, imgData["height"]?.value as Float)
-//        personImageActor.backgroundHandle
-        val imgData = person.get<OnjObject>("image")
-        println(imgData)
-//        personImageActor.setPosition(imgData.get<OnjFloat>("positionLeft").value.toFloat(),imgData.get<OnjFloat>("positionTop").value.toFloat())
-        personImageActor.setSize(200.0F,700.0F)
-//        personImageActor.setPosition(200F,500F)
+        personImageActor.setSize(200.0F, 700.0F)
 
         println("" + personImageActor.x + "  " + personImageActor.y)
         println("" + personImageActor.width + "  " + personImageActor.height)
-//        val rnd= Random(1)
-//        val shopOnj = shop.get<OnjObject>("shop")
-//        val shop2 = shop.readFromOnj(shopOnj, screen)
-//        shopWidget.start(shop2)
         println(personImageActor.parent.children)
 
-        onjScreen.addEarlyRenderTask(renderTask)
-
-    }
-
-    //    override fun update() {
-//        super.update()
-//        personImageActor.setSize(200.0F,700.0F)
-//        personImageActor.setPosition(200F,500F)
-//        personImageActor.isVisible=true
-//        personImageActor.zIndex=2
-//    }
-
-    private val renderTask: (Batch) -> Unit = { batch ->
-        personImageActor.draw(batch, 0F)
+//        personDrawable = ResourceManager.get(screen, imgData.get<String>("textureName"))
     }
 
     override fun update() {
         super.update()
-        println(personImageActor.width)
-//        System.exit(0)
-        personImageActor.setSize(200F,800F)
-        personImageActor.isVisible=true
+//        val imgData = person.get<OnjObject>("image")
+//        if (personImageActor.width != imgData.get<OnjFloat>("width").value.toFloat()) {
+//            personImageActor.width = imgData.get<OnjFloat>("width").value.toFloat()
+//            personImageActor.height = imgData.get<OnjFloat>("height").value.toFloat()
+//            personImageActor.x = imgData.get<OnjFloat>("positionLeft").value.toFloat()
+//            personImageActor.y = imgData.get<OnjFloat>("positionTop").value.toFloat()
+//            personImageActor.drawable = personDrawable
+//        }
     }
+
+//    override fun update() {
+//        super.update()
+//        println(personImageActor.width)
+////        System.exit(0)
+//        personImageActor.setSize(200F,800F)
+//        personImageActor.isVisible=true
+//    }
 
     companion object {
 
