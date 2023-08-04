c8dd40926f8347ee10906c823a595fab683fe435
felix <9202@htl.rennweg.at>
2023-08-04 09:11:12 +0200

    refactored Drag and Dop Payloads to be in separate files for readability

--- core/src/com/fourinachamber/fortyfive/game/card/CardDragAndDrop.kt
+++ core/src/com/fourinachamber/fortyfive/game/card/CardDragAndDrop.kt
@@ -13,63 +13,6 @@ import com.fourinachamber.fortyfive.utils.obj
 import onj.value.OnjNamedObject
 import kotlin.math.max
 
-/**
- * drags the object in the center
- */
-open class CenterDragged(
-    dragAndDrop: DragAndDrop,
-    actor: Actor,
-    onj: OnjNamedObject,
-) : DragBehaviour(dragAndDrop, actor, onj) {
-    override fun dragStart(event: InputEvent?, x: Float, y: Float, pointer: Int): Payload? {
-        return null
-    }
-
-    override fun drag(event: InputEvent?, x: Float, y: Float, pointer: Int) {
-        super.drag(event, x, y, pointer)
-        val parentOff = actor.parent.localToStageCoordinates(Vector2(0f, 0f))
-        dragAndDrop.setDragActorPosition(
-            -parentOff.x + actor.width / 2,
-            -parentOff.y - actor.height / 2
-        )
-        //if there are any errors, it might be because of scaling //see files before commit 17278a0ddd6f821358af53ba331443958292d872
-    }
-
-    override fun dragStop(
-        event: InputEvent?,
-        x: Float,
-        y: Float,
-        pointer: Int,
-        payload: Payload?,
-        target: DragAndDrop.Target?
-    ) {
-        if (payload == null) return
-        val obj = payload.obj as ExecutionPayload
-        obj.onDragStop()
-    }
-}
-
-open class ExecutionPayload { //TODO maybe interface, but idk how to set "tasks" default value to mutableListOf()
-
-    /**
-     * get executed on DragStop
-     */
-    val tasks: MutableList<() -> Unit> = mutableListOf()
-
-    /**
-     * called when the drag is stopped
-     */
-    fun onDragStop() {
-        for (task in tasks) task()
-    }
-
-    /**
-     * when the drag is stopped, the actor will be reset to [pos]
-     */
-    fun resetTo(actor: Actor, pos: Vector2) = tasks.add {
-        actor.setPosition(pos.x, pos.y)
-    }
-}
 
 
 /**
@@ -169,58 +112,5 @@ class CardDragAndDropPayload(val card: Card) : ExecutionPayload() {
      */
     fun loadIntoRevolver(slot: Int) = tasks.add {
         FortyFive.currentGame!!.loadBulletInRevolver(card, slot)  //TODO ugly
-    }
-}
-
-class ShopDragSource(
-    dragAndDrop: DragAndDrop,
-    actor: Actor,
-    onj: OnjNamedObject,
-) : CenterDragged(dragAndDrop, actor, onj) {
-
-    private val toLast: Boolean
-
-    init {
-        toLast = onj.getOr("moveToLastIndex", false)
-    }
-
-    override fun dragStart(event: InputEvent?, x: Float, y: Float, pointer: Int): Payload? {
-        if ((actor !is CustomImageActor) || (actor as CustomImageActor).inActorState("unbuyable")) return null
-        val payload = Payload()
-        dragAndDrop.setKeepWithinStage(false)
-
-        payload.dragActor = actor
-        (actor.parent.parent as CustomScrollableFlexBox).currentlyDraggedChild = actor.parent
-        if (toLast) actor.toFront()
-
-        val obj = ShopPayload(actor)
-        payload.obj = obj
-        obj.resetTo(actor,Vector2(actor.x, actor.y))
-        return payload
-    }
-
-    override fun dragStop(
-        event: InputEvent?,
-        x: Float,
-        y: Float,
-        pointer: Int,
-        payload: Payload?,
-        target: DragAndDrop.Target?
-    ) {
-        if (payload == null) return
-        if (toLast) actor.zIndex = max(actor.zIndex - 1, 0)
-        val obj = payload.obj as ShopPayload
-        (actor.parent.parent as CustomScrollableFlexBox).currentlyDraggedChild = null
-        obj.onDragStop()
-    }
-    class ShopPayload(val actor: Actor) : ExecutionPayload() {
-
-        /**
-         * called when the drag is stopped
-         */
-        fun onBuy() = tasks.add {
-            val scr = (FortyFive.screen as OnjScreen).screenController as ShopScreenController
-            scr.buyCard(actor)
-        }
     }
 }
