8953a24656fad142e97d3161f24dd50e9d9ac813
Marvin Kurka <8013@htl.rennweg.at>
2023-08-12 13:37:49 +0200

    started working on website

---
+++ website/index.js
@@ -0,0 +1,37 @@
+
+const scrollSpeed = 20;
+const numPages = 3;
+
+let currentPage = 1;
+let scrollToPosition = null;
+
+function main() {
+    document.getElementById("prev-page-button").addEventListener("click", prevPage);
+    document.getElementById("next-page-button").addEventListener("click", nextPage);
+    updateScroll();
+}
+
+function nextPage() {
+    if (currentPage >= numPages) return;
+    if (scrollToPosition != null) return;
+    currentPage++;
+    updateScroll();
+}
+
+function prevPage() {
+    if (currentPage <= 1) return;
+    if (scrollToPosition != null) return;
+    currentPage--;
+    updateScroll();
+}
+
+function updateScroll() {
+    const pageWidth = document.documentElement.scrollWidth / 3;
+    window.scroll({
+        top: 0,
+        left: pageWidth * (currentPage - 1),
+        behavior: "smooth"
+    });
+}
+
+window.onload = main;
