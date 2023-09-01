1c922f1ee8e7fb1bb89cc6e0e291944a323d22af
Marvin Kurka <8013@htl.rennweg.at>
2023-09-01 15:22:11 +0200

    worked on hamburger menu

--- website/index.js
+++ website/index.js
@@ -7,22 +7,74 @@ let scrollToPosition = null;
 
 let lastYScrollPosition = 0;
 
-function main() {
-    bindEventListeners(document.getElementsByClassName("to-main-page"), 1);
-    bindEventListeners(document.getElementsByClassName("to-about-page"), 2);
-    bindEventListeners(document.getElementsByClassName("to-imprint-page"), 3);
+let navbarDependents;
+let navbarOpen = false;
 
+let curPageDependents;
+
+function main() {
+    doPageChangeListeners();
+    doNavbarListeners();
+    updateCurPageClasses();
+
+    // fix scroll offset when resizing
     addEventListener("scroll", () => lastYScrollPosition = window.scrollY);
     addEventListener("resize", () => fixScroll());
+
     // updateScroll();
 }
 
-function bindEventListeners(elements, pageNumber) {
+function doNavbarListeners() {
+    navbarDependents = Array.from(document.getElementsByClassName("navbar-dependent"));
+    addEventListener("click", () => {
+        closeNavbar();
+    });
+    Array.from(document.getElementsByClassName("opens-navbar")).forEach(element => {
+        element.addEventListener("click", e => {
+            if (navbarOpen) return;
+            e.stopPropagation();
+            openNavbar();
+        });
+    });
+}
+
+function openNavbar() {
+    if (navbarOpen) return;
+    navbarOpen = true;
+    navbarDependents.forEach(element => element.classList.add("navbar-open"));
+}
+
+function closeNavbar() {
+    if (!navbarOpen) return;
+    navbarOpen = false;
+    navbarDependents.forEach(element => element.classList.remove("navbar-open"));
+}
+
+
+function doPageChangeListeners() {
+    bindScrollEventListeners(document.getElementsByClassName("to-main-page"), 1);
+    bindScrollEventListeners(document.getElementsByClassName("to-about-page"), 2);
+    bindScrollEventListeners(document.getElementsByClassName("to-imprint-page"), 3);
+
+    curPageDependents = Array.from(document.getElementsByClassName("cur-page-dependent"));
+}
+
+function bindScrollEventListeners(elements, pageNumber) {
     Array.from(elements).forEach(element => {
         element.addEventListener("click", () => {
             currentPage = pageNumber;
+            updateCurPageClasses();
             updateScroll();
         });
+    });
+}
+
+function updateCurPageClasses() {
+    curPageDependents.forEach(element => {
+        for (let i = 1; i <= numPages; i++) {
+            element.classList.remove(`cur-page-${i}`)
+        }
+        element.classList.add(`cur-page-${currentPage}`)
     });
 }
 
