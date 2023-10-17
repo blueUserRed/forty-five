5a85d526e25a77075e862b89cf07af6524f0e499
Marvin Kurka <8013@htl.rennweg.at>
2023-10-17 13:22:40 +0200

    worked on the moist encounter modifier

--- core/src/com/fourinachamber/fortyfive/game/EncounterModifier.kt
+++ core/src/com/fourinachamber/fortyfive/game/EncounterModifier.kt
@@ -2,6 +2,7 @@ package com.fourinachamber.fortyfive.game
 
 import com.fourinachamber.fortyfive.game.GameController.RevolverRotation
 import com.fourinachamber.fortyfive.game.card.Card
+import com.fourinachamber.fortyfive.utils.TemplateString
 import com.fourinachamber.fortyfive.utils.Timeline
 import java.lang.Exception
 
@@ -32,18 +33,83 @@ sealed class EncounterModifier {
         ): Timeline = controller.tryToPutCardsInHandTimeline(card.name)
     }
 
+    class Moist : EncounterModifier() {
+
+        private val cardModifiers: MutableList<Card.CardModifier> = mutableListOf()
+
+        override fun executeAfterBulletWasPlacedInRevolver(
+            card: Card,
+            controller: GameController
+        ): Timeline = Timeline.timeline {
+            action {
+                val modifier = Card.CardModifier(
+                    0,
+                    null,
+                    validityChecker = { true }
+                )
+                card.addModifier(modifier)
+                cardModifiers.add(modifier)
+            }
+        }
+
+        override fun executeAfterRevolverWasShot(
+            card: Card?,
+            controller: GameController
+        ): Timeline = Timeline.timeline {
+            action {
+                card ?: return@action
+                val modifier = cardModifiers.firstOrNull { it in card.modifiers } ?: return@action
+                cardModifiers.remove(modifier)
+            }
+        }
+
+        override fun executeAfterRevolverRotated(
+            rotation: RevolverRotation,
+            controller: GameController
+        ): Timeline = Timeline.timeline {
+            action {
+                controller
+                    .revolver
+                    .slots
+                    .mapNotNull { it.card }
+                    .forEach { updateModifierOfCard(it, rotation) }
+            }
+        }
+
+        private fun updateModifierOfCard(card: Card, rotation: RevolverRotation) {
+            // TODO: oldModifier == null for some reason
+            val oldModifier = cardModifiers.firstOrNull { it in card.modifiers }
+            val damage = (oldModifier?.damage ?: 0) - rotation.amount
+            val newModifier = Card.CardModifier(
+                damage,
+                TemplateString("Bullet lost $damage damage due to the Moist Encounter Modifier"),
+                validityChecker = { true }
+            )
+            println("update modifier: $card, rotation: $rotation, damage: $damage")
+            oldModifier?.let { card.removeModifier(it) }
+            card.addModifier(newModifier)
+        }
+
+    }
+
     open fun modifyRevolverRotation(rotation: RevolverRotation): RevolverRotation = rotation
 
     open fun shouldApplyStatusEffects(): Boolean = true
 
     open fun executeAfterBulletWasPlacedInRevolver(card: Card, controller: GameController): Timeline? = null
 
+    open fun executeAfterRevolverWasShot(card: Card?, controller: GameController): Timeline? = null
+
+    open fun executeAfterRevolverRotated(rotation: RevolverRotation, controller: GameController): Timeline? = null
+
     companion object {
+
         fun getFromName(name: String) = when (name.lowercase()) {
             "rain" -> Rain
             "frost" -> Frost
             "bewitchedmist" -> BewitchedMist
             "lookalike" -> Lookalike
+            "moist" -> Moist()
             else -> throw Exception("Unknown Encounter Modifier: ${name.lowercase()}")
         }
     }
