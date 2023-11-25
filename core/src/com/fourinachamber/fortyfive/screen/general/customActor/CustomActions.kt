6dc41bf99f19276d7fe75f4b35da06addb2956c2
felix <9202@htl.rennweg.at>
2023-11-25 18:21:54 +0100

    added a close button for the normal warnings (permas might be broken)

--- core/src/com/fourinachamber/fortyfive/screen/general/customActor/CustomActions.kt
+++ core/src/com/fourinachamber/fortyfive/screen/general/customActor/CustomActions.kt
@@ -1,6 +1,8 @@
 package com.fourinachamber.fortyfive.screen.general.customActor
 
 import com.badlogic.gdx.math.Interpolation
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.actions.RelativeTemporalAction
 import com.fourinachamber.fortyfive.screen.general.CustomFlexBox
 import com.fourinachamber.fortyfive.screen.general.OffSettable
@@ -24,11 +26,57 @@ class CustomMoveByAction(
 
     override fun update(interpol: Float) {
         super.update(interpol)
-        val target = this.target
-        target as CustomFlexBox
+        val target = this.target ?: return
+        target as OffSettable
         val curDist = interpol - lastDist
         target.offsetX += curDist * relX
         target.offsetY += curDist * relY
         lastDist = interpol
     }
 }
+
+//
+//class CustomMoveToAction(
+//    var target: CustomFlexBox? = null,
+//    interpolation: Interpolation = Interpolation.linear,
+//    private val startX: Float? = null,
+//    private val startY: Float? = null,
+//    private val endX: Float? = null,
+//    private val endY: Float? = null,
+//    duration: Float = 1000F,
+//) : RelativeTemporalAction() {
+//    init {
+//        super.setInterpolation(interpolation)
+//        super.setDuration(duration / 1000)
+//    }
+//
+//    private var lastDist = 0F
+//
+//    override fun updateRelative(percentDelta: Float) {
+//    }
+//
+//    private var distX: Float? = null
+//    private var distY: Float? = null
+//    override fun begin() {
+//        super.begin()
+//        distX = if (endX != null) {
+//            if (startX != null) target.x = startX!!
+//            startX!! - target.x
+//        } else null
+//
+//        distY = if (endY != null) {
+//            if (startY != null) target.y = startY!!
+//            startY!! - target.y
+//        } else null
+//    }
+//
+//    override fun update(interpol: Float) {
+//        super.update(interpol)
+//        val target = this.target ?: return
+//        target as CustomFlexBox
+//        val curDist = interpol - lastDist
+//        target.x += curDist * (distX ?: 0F)
+//        target.y += curDist * (distY ?: 0F)
+//        lastDist = interpol
+//    }
+//}
