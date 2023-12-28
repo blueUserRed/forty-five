0c08df39c4519b6b84aac9494859d89a93a42439
Marvin Kurka <8013@htl.rennweg.at>
2023-12-28 10:12:48 +0100

    fixed issue #53, some general QoL for map navigation

--- core/src/com/fourinachamber/fortyfive/screen/general/ScreenBuilder.kt
+++ core/src/com/fourinachamber/fortyfive/screen/general/ScreenBuilder.kt
@@ -463,7 +463,7 @@ class ScreenBuilder(val file: FileHandle) {
             widgetOnj.get<String>("encounterModifierParentName"),
             widgetOnj.get<String>("encounterModifierDisplayTemplateName"),
             widgetOnj.get<Double>("screenSpeed").toFloat(),
-//            widgetOnj.get<Double>("backgroundScale").toFloat(),
+            widgetOnj.get<Double>("scrollMargin").toFloat(),
             widgetOnj.get<Double>("disabledDirectionIndicatorAlpha").toFloat(),
             widgetOnj.get<Double>("mapScale").toFloat(),
         )
