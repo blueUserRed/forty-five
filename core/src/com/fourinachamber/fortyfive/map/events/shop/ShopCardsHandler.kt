0597509f034d75f7443d8c0d0b865b6d29713db7
felix <9202@htl.rennweg.at>
2024-02-15 17:32:30 +0100

    mapchanges with phillipp

--- core/src/com/fourinachamber/fortyfive/map/events/shop/ShopCardsHandler.kt
+++ core/src/com/fourinachamber/fortyfive/map/events/shop/ShopCardsHandler.kt
@@ -26,7 +26,7 @@ class ShopCardsHandler(
     private val labels: MutableList<CustomLabel> = mutableListOf()
 
     fun addItems(rnd: Random, contextTypes: Set<String>, defaultType: String) {
-        val nbrOfItems = (5..10).random(rnd)
+        val nbrOfItems = (9..11).random(rnd)
         FortyFiveLogger.debug(logTag, "Creating $nbrOfItems items")
         val cardsToAdd = RandomCardSelection.getRandomCards(
             screen,
