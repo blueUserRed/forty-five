7685c7303a89d445f5ba064442e32292ddf6e39b
Marvin Kurka <8013@htl.rennweg.at>
2024-02-23 10:26:39 +0100

    continued working on the title screen

--- core/src/com/fourinachamber/fortyfive/screen/gameComponents/TitleScreenController.kt
+++ core/src/com/fourinachamber/fortyfive/screen/gameComponents/TitleScreenController.kt
@@ -1,11 +1,14 @@
 package com.fourinachamber.fortyfive.screen.gameComponents
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.utils.TimeUtils
+import com.fourinachamber.fortyfive.FortyFive
 import com.fourinachamber.fortyfive.screen.SoundPlayer
-import com.fourinachamber.fortyfive.screen.general.OnjScreen
-import com.fourinachamber.fortyfive.screen.general.ScreenController
+import com.fourinachamber.fortyfive.screen.general.*
 import com.fourinachamber.fortyfive.screen.general.customActor.OffSettable
+import com.fourinachamber.fortyfive.utils.TemplateString
+import com.fourinachamber.fortyfive.utils.Timeline
 import kotlin.math.sin
 
 class TitleScreenController : ScreenController() {
@@ -13,9 +16,50 @@ class TitleScreenController : ScreenController() {
     private lateinit var screen: OnjScreen
 
     private var transitionAwayVelocity: Float = -1f
+    private val timeline: Timeline = Timeline()
+
+    private var isConfirmed: Boolean = false
 
     override fun init(onjScreen: OnjScreen, context: Any?) {
         screen = onjScreen
+        timeline.startTimeline()
+    }
+
+    override fun onUnhandledEvent(event: Event) = when (event) {
+
+        is QuitGameEvent -> timeline.appendAction(Timeline.timeline {
+            action {
+                screen.enterState(showConfirmationPopupScreenState)
+                TemplateString.updateGlobalParam("title_screen.popupText", "Are you sure you want to quit?")
+            }
+            delayUntil { isConfirmed || showConfirmationPopupScreenState !in screen.screenState }
+            action {
+                if (isConfirmed) Gdx.app.exit()
+            }
+
+        }.asAction())
+
+        is AbandonRunEvent -> timeline.appendAction(Timeline.timeline {
+            action {
+                screen.enterState(showConfirmationPopupScreenState)
+                TemplateString.updateGlobalParam("title_screen.popupText", "Are you sure you want to abandon" +
+                        " your run? You will loose all the progress you made and all bullets that haven't been saved yet.")
+            }
+            delayUntil { isConfirmed || showConfirmationPopupScreenState !in screen.screenState }
+            action {
+                if (isConfirmed) FortyFive.newRun(false)
+                isConfirmed = false
+                screen.leaveState(showConfirmationPopupScreenState)
+            }
+
+        }.asAction())
+
+        is PopupConfirmationEvent -> {
+            isConfirmed = true
+        }
+
+        else -> {}
+
     }
 
     private fun doTransitionAwayAnim() {
@@ -28,6 +72,7 @@ class TitleScreenController : ScreenController() {
     }
 
     override fun update() {
+        timeline.updateTimeline()
         if (transitionAwayVelocity != -1f) {
             doTransitionAwayAnim()
             return
@@ -47,4 +92,9 @@ class TitleScreenController : ScreenController() {
             actor.offsetY = sin(TimeUtils.millis() * 0.001 + i * i * 100).toFloat() * 6f
         }
     }
+
+    companion object {
+        const val showConfirmationPopupScreenState = "show_confirmation_popup"
+    }
+
 }
