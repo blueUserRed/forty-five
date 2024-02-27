ee4b75e40453fe1b47155479817c3fd6eaa0d6b9
Marvin Kurka <8013@htl.rennweg.at>
2024-02-27 15:43:19 +0100

    minor fixes

--- core/src/com/fourinachamber/fortyfive/screen/gameComponents/TitleScreenController.kt
+++ core/src/com/fourinachamber/fortyfive/screen/gameComponents/TitleScreenController.kt
@@ -26,7 +26,7 @@ class TitleScreenController : ScreenController() {
         timeline.startTimeline()
         TemplateString.updateGlobalParam(
             "title_screen.startButtonText",
-            if (SaveState.playerCompletedFirstTutorialEncounter) "Continue" else "Start you journey"
+            if (SaveState.playerCompletedFirstTutorialEncounter) "Continue" else "Start your Journey"
         )
     }
 
