4af81131ff714ea7ec1c6610f39f85431fe5d543
Marvin Kurka <8013@htl.rennweg.at>
2024-02-27 15:43:19 +0100

    minor fixes

--- core/src/com/fourinachamber/fortyfive/game/StatsScreenController.kt
+++ core/src/com/fourinachamber/fortyfive/game/StatsScreenController.kt
@@ -9,7 +9,7 @@ import com.fourinachamber.fortyfive.utils.TemplateString
 class StatsScreenController : ScreenController() {
 
     override fun init(onjScreen: OnjScreen, context: Any?) {
-        val cards = SaveState.cards
+        val cards = PermaSaveState.statCardsLastRun
         val prototypes = RandomCardSelection.allCardPrototypes
         TemplateString.updateGlobalParam("stat.bulletsCollected", cards.distinct().size)
         val obtainableBullets = prototypes
