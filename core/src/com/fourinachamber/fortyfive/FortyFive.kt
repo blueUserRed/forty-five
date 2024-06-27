e26e878232c9d43aa13d45edb337d10349669952
Marvin Kurka <8013@htl.rennweg.at>
2024-06-27 17:16:48 +0200

    added new smaller mountains to the magenta mountains

--- core/src/com/fourinachamber/fortyfive/FortyFive.kt
+++ core/src/com/fourinachamber/fortyfive/FortyFive.kt
@@ -155,7 +155,7 @@ object FortyFive : Game() {
         GameDirector.init()
         MapManager.init()
 //        resetAll()
-//        MapManager.generateMapsSync()
+        MapManager.generateMapsSync()
 //        newRun(false)
 
         if (!Gdx.files.internal("saves/perma_savefile.onj").file().exists()) {
