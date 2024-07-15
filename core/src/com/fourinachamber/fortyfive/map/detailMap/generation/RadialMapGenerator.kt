42c03efca44ded546b37c5866ec9af904c504e68
Marvin Kurka <8013@htl.rennweg.at>
2024-07-15 21:06:49 +0200

    cleaned up RadialMapGenerator

--- core/src/com/fourinachamber/fortyfive/map/detailMap/generation/RadialMapGenerator.kt
+++ core/src/com/fourinachamber/fortyfive/map/detailMap/generation/RadialMapGenerator.kt
@@ -1,10 +1,10 @@
 package com.fourinachamber.fortyfive.map.detailMap.generation
 
 import com.badlogic.gdx.math.Vector2
-import com.fourinachamber.fortyfive.map.detailMap.DetailMap
-import com.fourinachamber.fortyfive.map.detailMap.MapNodeBuilder
-import com.fourinachamber.fortyfive.utils.minus
-import com.fourinachamber.fortyfive.utils.random
+import com.fourinachamber.fortyfive.map.detailMap.*
+import com.fourinachamber.fortyfive.utils.*
+import onj.value.OnjArray
+import onj.value.OnjNamedObject
 import onj.value.OnjObject
 import kotlin.math.cos
 import kotlin.math.sin
@@ -15,13 +15,19 @@ class RadialMapGenerator(val data: RadialMapGeneratorData) : BaseMapGenerator() 
         setup(name, data)
         val startNode = newNode(0f, 0f)
 
-        val circles = listOf(
-            Circle(20f, 3),
-            Circle(40f, 6),
-            Circle(60f, 9)
-        )
-        val nodes = generateNodes(circles)
-        connectNodes(startNode, nodes)
+        val nodes = generateNodes(data.circles)
+        generateNodeConnections(startNode, nodes)
+
+        setupBounds(data.horizontalExtension, data.verticalExtension)
+
+        val (animatedDecorations, decorations) = data
+            .decorations
+            .splitInTwo { it.animated }
+
+        assignEvents(data.events, nodes)
+
+        val genDecorations = decorations.map { generateDecoration(it) }
+        val genAnimatedDecorations = animatedDecorations.map { generateDecoration(it) }
 
         startNode.build()
 
@@ -29,10 +35,8 @@ class RadialMapGenerator(val data: RadialMapGeneratorData) : BaseMapGenerator() 
             name = name,
             startNode = startNode.asNode!!,
             endNode = allNodes.last().asNode!!,
-            decorations = listOf(),
-            animatedDecorations = listOf(),
-//            decorations = genDecorations,
-//            animatedDecorations = genAnimatedDecorations,
+            decorations = genDecorations,
+            animatedDecorations = genAnimatedDecorations,
             isArea = false,
             biome = data.biome,
             progress = 0f..10f,
@@ -42,11 +46,54 @@ class RadialMapGenerator(val data: RadialMapGeneratorData) : BaseMapGenerator() 
         )
     }
 
+    private fun assignEvents(events: List<RadialMapGeneratorEventSpawner>, nodes: List<List<MapNodeBuilder>>) {
+        nodes.forEachIndexed { index, circle ->
+            val possibleEvents = events
+                .filter { it.circle == null || it.circle == index }
+                .filter { it.fixedAmount == null }
+            if (possibleEvents.isEmpty()) return@forEachIndexed
+            circle.forEach { node ->
+                val chosen = possibleEvents
+                    .map { it.weight to it }
+                    .weightedRandom(random)
+                node.event = chosen.eventCreator()
+                node.nodeTexture = chosen.nodeTexture
+            }
+        }
+        val allNodes = nodes.flatten()
+        val fixedEvents = events.filter { it.fixedAmount != null }
+        val usedNodes = mutableListOf<MapNodeBuilder>()
+        fixedEvents.forEach { event ->
+            val possibleNodes = if (event.circle == null) {
+                allNodes
+            } else {
+                nodes.getOrNull(event.circle) ?: throw RuntimeException("no circle with index ${event.circle}")
+            }
+            var spawned = 0
+            while (true) {
+                if (spawned >= event.fixedAmount!!) break
+                val candidates = possibleNodes.filter { it !in usedNodes }
+                if (candidates.isEmpty()) {
+                    FortyFiveLogger.warn(
+                        logTag,
+                        "Cant spawn event $event ${event.fixedAmount} times because no possible nodes are left"
+                    )
+                    break
+                }
+                val chosen = candidates.random(random)
+                chosen.event = event.eventCreator()
+                chosen.nodeTexture = event.nodeTexture
+                usedNodes.add(chosen)
+                spawned++
+            }
+        }
+    }
+
     private fun generateNodes(circles: List<Circle>): List<List<MapNodeBuilder>> = circles.map { circle ->
         val amountNodes = circle.numNodes
         val anglePerNode = Math.PI * 2 / amountNodes
         val shift = (0f..(Math.PI.toFloat() * 2f)).random(random)
-        val variance = -0.001f..0.001f
+        val variance = (-circle.angleVariance)..(circle.angleVariance)
         val nodes = mutableListOf<MapNodeBuilder>()
         repeat(amountNodes) { i ->
             val angle = (anglePerNode * i + shift + variance.random(random)).toFloat()
@@ -56,7 +103,7 @@ class RadialMapGenerator(val data: RadialMapGeneratorData) : BaseMapGenerator() 
         nodes
     }
 
-    private fun connectNodes(startNode: MapNodeBuilder, nodes: List<List<MapNodeBuilder>>) {
+    private fun generateNodeConnections(startNode: MapNodeBuilder, nodes: List<List<MapNodeBuilder>>) {
         val circles = mutableListOf(
             listOf(startNode)
         )
@@ -84,17 +131,30 @@ class RadialMapGenerator(val data: RadialMapGeneratorData) : BaseMapGenerator() 
         y = cos(angle) * radius
     )
 
-    private fun randomAngle(): Float = (0f..(2f * Math.PI.toFloat())).random(random)
-
     data class Circle(
         val radius: Float,
         val numNodes: Int,
-    )
+        val angleVariance: Float
+    ) {
+        companion object {
+
+            fun fromOnj(onj: OnjObject) = Circle(
+                radius = onj.get<Double>("radius").toFloat(),
+                numNodes = onj.get<Long>("numNodes").toInt(),
+                angleVariance = onj.get<Double>("angleVariance").toFloat(),
+            )
+        }
+    }
 
     data class RadialMapGeneratorData(
         override val seed: Long,
         override val nodeProtectedArea: Float,
-        val biome: String
+        val biome: String,
+        val circles: List<Circle>,
+        val horizontalExtension: Float,
+        val verticalExtension: Float,
+        val decorations: List<MapGeneratorDecoration>,
+        val events: List<RadialMapGeneratorEventSpawner>,
     ) : BaseMapGeneratorData {
 
         companion object {
@@ -102,7 +162,40 @@ class RadialMapGenerator(val data: RadialMapGeneratorData) : BaseMapGenerator() 
             fun fromOnj(onj: OnjObject) = RadialMapGeneratorData(
                 seed = onj.get<Long>("seed"),
                 nodeProtectedArea = onj.get<Double>("nodeProtectedArea").toFloat(),
-                biome = onj.get<String>("biome")
+                biome = onj.get<String>("biome"),
+                horizontalExtension = onj.get<Double>("horizontalExtension").toFloat(),
+                verticalExtension = onj.get<Double>("verticalExtension").toFloat(),
+                circles = onj
+                    .get<OnjArray>("circles")
+                    .value
+                    .map { Circle.fromOnj(it as OnjObject) },
+                decorations = onj
+                    .get<OnjArray>("decorations")
+                    .value
+                    .map { MapGeneratorDecoration.fromOnj(it as OnjObject) },
+                events = onj
+                    .get<OnjArray>("events")
+                    .value
+                    .map { RadialMapGeneratorEventSpawner.fromOnj(it as OnjObject) }
+            )
+        }
+    }
+
+    data class RadialMapGeneratorEventSpawner(
+        val eventCreator: () -> MapEvent,
+        val circle: Int?,
+        val weight: Int,
+        val nodeTexture: String,
+        val fixedAmount: Int?,
+    ) {
+        companion object {
+
+            fun fromOnj(onj: OnjObject) = RadialMapGeneratorEventSpawner(
+                eventCreator = { MapEventFactory.getMapEvent(onj.get<OnjNamedObject>("event")) },
+                nodeTexture = onj.get<String>("nodeTexture"),
+                circle = onj.getOr<Long?>("circle", null)?.toInt(),
+                weight = onj.get<Long>("weight").toInt(),
+                fixedAmount = onj.getOr<Long?>("fixedAmount", null)?.toInt(),
             )
         }
     }
