af8b15a07c7188e1ba3b6a4c5f11e81ba6372058
felix <9202@htl.rennweg.at>
2024-09-09 18:01:56 +0200

    changed package name for some classes

--- core/src/com/fourinachamber/fortyfive/screen/Screens/HealOrMaxHPScreen.kt
+++ core/src/com/fourinachamber/fortyfive/screen/Screens/HealOrMaxHPScreen.kt
@@ -1,4 +1,4 @@
-package com.fourinachamber.fortyfive.screen.Screens
+package com.fourinachamber.fortyfive.screen.screens
 
 import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.Touchable
--- core/src/com/fourinachamber/fortyfive/screen/Screens/ShopScreen.kt
+++ core/src/com/fourinachamber/fortyfive/screen/Screens/ShopScreen.kt
@@ -1,4 +1,4 @@
-package com.fourinachamber.fortyfive.screen.Screens
+package com.fourinachamber.fortyfive.screen.screens
 
 import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.scenes.scene2d.Group
