8ecbf5e5bfce1de860c7be6c422017e1a72bfed3
Marvin Kurka <8013@htl.rennweg.at>
2025-06-25 19:11:08 +0200

    deleted comments in shader code

--- assets/shaders/drop_shadow.glsl
+++ assets/shaders/drop_shadow.glsl
@@ -21,11 +21,6 @@ uniform float u_brighten;
 
 %include shaders/includes/color_utils.glsl
 
-//float getGauss(int i, int j){
-//    return 1.0;
-//    //    return pow(2.7182,-float(i*i+j*j)/4.5); //gauss but already simplified with sigma=1.5
-//}
-
 vec4 adjustedSample(vec2 at) {
     float multiplier = 1.0 + u_multiplier;
     float invMultiplier = 1.0 / multiplier;
@@ -94,55 +89,3 @@ void main() {
 
     outColor = mixed;
 }
-
-
-//void main() {
-//    int depthPerDist = 50;
-//    int maxSum = int(float(depthPerDist) * 1.6);
-//
-//    float distancePerDirection = u_multiplier/(u_multiplier * 2.0 + 1.0);
-//    float stepDist = distancePerDirection/float(depthPerDist);
-//
-//    float alpha = 0.0;
-//
-//    float multi = (1.0 + u_multiplier * 2.0);
-//    vec2 startPos = (v_texCoords.xy + vec2(float(-depthPerDist) * stepDist, float(-depthPerDist) * stepDist)) * multi  - u_multiplier;
-//    vec2 endPos = (v_texCoords.xy + vec2(float(depthPerDist) * stepDist, float(depthPerDist) * stepDist)) * multi  - u_multiplier;
-//    vec2 transformedStep = (endPos - startPos) / (depthPerDist * 2.0);
-//    startPos = max(startPos, vec2(0.0));
-//    endPos = min(endPos, vec2(1.0));
-//    for (float i = startPos.x; i <= endPos.x; i += transformedStep.x){
-//        for (float j = startPos.y; j <= endPos.y; j += transformedStep.y){
-//            alpha += texture2D(u_texture, vec2(i, j)).a;
-//        }
-//    }
-//    //    for (int i=-depthPerDist;i<=depthPerDist;i++){ //THIS IS THE OLD VERSION
-//    //        for (int j=-depthPerDist;j<=depthPerDist;j++){
-//    //            vec2 calcPos = (v_texCoords.xy + vec2(float(i) * stepDist, float(j) * stepDist)) * multi  - u_multiplier;
-//    //            if (calcPos.x >= 0.0 && calcPos.y >= 0.0 && calcPos.x <= 1.0 && calcPos.y <= 1.0) alpha += texture2D(u_texture, calcPos).a;
-//    //        }
-//    //    }
-//    float totalSum = float((depthPerDist * 2) * (depthPerDist * 2));
-//    float alphaNew = alpha / totalSum * 2.0  * u_maxOpacity;
-//    if (alphaNew >= 1.0){
-//        alphaNew = 1.0;
-//    }
-//    outColor = vec4(u_color.xyz, alphaNew);
-//}
-
-
-//--------------------OLD CODE FOR TESTING AND WASN'T SO BAD AS A BACKUP
-//    float distX = (v_texCoords.x-0.5)*(v_texCoords.x-0.5);
-//    float distY = (v_texCoords.y-0.5)*(v_texCoords.y-0.5);
-////    float noise = snoise(v_texCoords*1000);
-//    float newAlpha=0.0;
-//
-//    if (distY<distX) newAlpha=(0.2 - distX);
-//    else newAlpha=(0.2 - distY);
-//    vec2 original=v_texCoords*1.4-0.2;
-//    float originalImageAlpha1 = texture(u_texture, vec2(original.x,v_texCoords.y)).a;
-//    float originalImageAlpha2 = texture(u_texture, vec2(v_texCoords.x,original.y)).a;
-//    if (originalImageAlpha1>originalImageAlpha2) newAlpha=originalImageAlpha1;
-//    else newAlpha=originalImageAlpha2;
-////    newAlpha=originalImageAlpha;
-//    gl_FragColor = vec4(1.0, 0.0, 0.0, newAlpha);
