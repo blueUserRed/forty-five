6a3fed541ea717da521ea51f101b172a20e02713
blueUserRed <-@->
2026-02-20 02:40:01 +0100

    remove skipping from timeline

--- core/src/com/microwavestudios/fortyfive/game/controller/GameControllerImpl.kt
+++ core/src/com/microwavestudios/fortyfive/game/controller/GameControllerImpl.kt
@@ -651,19 +651,16 @@ class GameControllerImpl(
         repeat(cardsToDraw) {
             include(drawCardTimeline(fromBottom, sourceCard, cardAcc))
         }
-
-        skipping { skip ->
-            action { if (cardsToDraw <= 0) skip() }
-            later {
-                val info = createTriggerInfo(
-                    null,
-                    amountOfCardsDrawn = cardsToDraw,
-                    sourceCard = sourceCard
-                )
-                val event = Events.CardsDrawnEvent(cardsToDraw, isSpecial, fromBottom, cardAcc, info)
-                gameEvents.fire(event)
-                include(event.createTimeline())
-            }
+        later {
+            if (cardsToDraw <= 0) return@later
+            val info = createTriggerInfo(
+                null,
+                amountOfCardsDrawn = cardsToDraw,
+                sourceCard = sourceCard
+            )
+            val event = Events.CardsDrawnEvent(cardsToDraw, isSpecial, fromBottom, cardAcc, info)
+            gameEvents.fire(event)
+            include(event.createTimeline())
         }
     } }
 
@@ -1278,10 +1275,11 @@ class GameControllerImpl(
     }
 
     private fun loadBulletFromHandInRevolverTimeline(card: Card, slot: Int): Timeline = Timeline.timeline {
-        skipping { skip ->
-            var cardInSlot: Card? = null
-            val info = createTriggerInfo(card, sourceCard = card)
-            val beforeEvent = Events.CardChangeZoneEvent(card, Zone.HAND, Zone.REVOLVER, before = true, info)
+        var skip = false
+        var cardInSlot: Card? = null
+        val info = createTriggerInfo(card, sourceCard = card)
+        val beforeEvent = Events.CardChangeZoneEvent(card, Zone.HAND, Zone.REVOLVER, before = true, info)
+        later {
             action {
                 FortyFive.logger.debug(logTag, "attempting to load bullet $card in revolver slot $slot")
                 cardInSlot = revolver.getCardInSlot(slot)
@@ -1291,8 +1289,11 @@ class GameControllerImpl(
                         || !tryPay(card.baseCost, card.actor)
                 if (!shouldSkip) return@action
                 FortyFive.soundPlayer.situation("not_allowed", screen)
-                skip()
+                skip = true
             }
+        }
+        later {
+            if (skip) return@later
             includeLater({
                 gameEvents.fire(beforeEvent)
                 beforeEvent.createTimeline()
